917a254a07b0778708bdf39a460212bcf6fb6837
Robert Elwell <robert@wikia-inc.com>
2013-10-29 12:38:42 -0700

    make it more obvious

--- testapp.py
+++ testapp.py
@@ -21,6 +21,14 @@ def get_topics_sorted(doc):
 def intersection_count(tuples1, tuples2):
     return len([x for x in tuples1 if x[0] in [y[0] for y in tuples2]])
 
+@app.template_filter('topics_sorted_keys')
+def get_topics_sorted(doc):
+    return sorted([key for key in doc.keys() if re.match(r'topic_\d+_tf', key) is not None], reverse=True, key=lambda x:x[1])
+
+@app.template_filter('append')
+def append(list, val):
+    return list + [val]
+
 
 def get_random_grouping():
     params = dict(rows=50, q='*:*', sort='wam_i desc', wt='json', fl='id,sitename_txt,topic_*,top_articles_mv_en')
