89fb5f9643405ec4efda0358fdf7b9d33e700410
Robert Elwell <robert@wikia-inc.com>
2013-10-29 16:54:00 -0700

    this test app is fucking awesome

--- testapp.py
+++ testapp.py
@@ -22,7 +22,7 @@ def intersection_count(tuples1, tuples2):
     return len([x for x in tuples1 if x[0] in [y[0] for y in tuples2]])
 
 @app.template_filter('topics_sorted_keys')
-def get_topics_sorted(doc):
+def get_topics_sorted_keys(doc):
     return sorted([key for key in doc.keys() if re.match(r'topic_\d+_tf', key) is not None], reverse=True, key=lambda x:x[1])
 
 @app.template_filter('append')
@@ -31,22 +31,22 @@ def append(list, val):
 
 
 def get_random_grouping():
-    params = dict(rows=50, q='*:*', sort='wam_i desc', wt='json', fl='id,sitename_txt,topic_*,top_articles_mv_en')
+    params = dict(rows=50, q='*:*', sort='wam_i desc', wt='json', fl='id,sitename_txt,topic_*,top_articles_mv_en,wam_i')
     docs = requests.get('%s/select/' % SOLR_URL, params=params).json().get('response', {}).get('docs', [])
     random.shuffle(docs)
     return docs
 
 
-def get_similar(query, wam_boost=None, topic_boost='1000', delta=0.15, naive=False, use_titles=False):
+def get_similar_old(query, wam_boost=None, topic_boost='1000', delta=0.15, naive=False, use_titles=False):
     doc_response = requests.get('%s/select/' % SOLR_URL, params=dict(rows=1, q='id:%s' % query, wt='json')).json()
     doc = doc_response.get('response', {}).get('docs', [None])[0]
     if doc is None:
-        return []  # same diff
+        return None, []  # same diff
 
     """
     Let's boost the query by the value of the topics, with boost
     """
-    bf = ['%s^%s' % (key, topic_boost) for key in doc.keys() if re.match(r'topic_\d+_tf', key) is not None]
+    bf = ['map(%s, 0, 100, 0, 0)^%s' % (key, topic_boost) for key in doc.keys() if re.match(r'topic_\d+_tf', key) is not None]
 
     if wam_boost is not None:
         bf += ['wam_i^%s' % wam_boost]
@@ -70,25 +70,64 @@ def get_similar(query, wam_boost=None, topic_boost='1000', delta=0.15, naive=Fal
         params['qf'] = "top_articles_mv_en,top_categories_mv_en,sitename_txt,description_en,headline_txt"
     """
 
-    params['boost'] = map(lambda x: re.sub(r'\^\d+', '', x), params['bf'])
-    del params['bf']
+    return (doc, requests.get('%s/select/' % SOLR_URL, params=params).json().get('response', {}).get('docs', []))
+
+def mlt(query):
+    doc_response = requests.get('%s/select/' % SOLR_URL, params=dict(rows=1, q='id:%s' % query, wt='json')).json()
+    doc = doc_response.get('response', {}).get('docs', [None])[0]
+    if doc is None:
+        return None, []  # same diff
+
+    params = {'wt':'json', 
+              'q':'-id:%s AND (%s)' % (doc['id'], " OR ".join(['(%s:*)' % key for key in get_topics_sorted_keys(doc)])),
+              'bf': ['%s^10' for key in get_topics_sorted_keys(doc)] + ['wam_i'],
+              'rows':20, 
+              'fl':'id,sitename_txt,topic_*,wam_i,url'}
 
     return (doc, requests.get('%s/select/' % SOLR_URL, params=params).json().get('response', {}).get('docs', []))
+
+
+def as_euclidean(query):
+    doc_response = requests.get('%s/select/' % SOLR_URL, params=dict(rows=1, q='id:%s' % query, wt='json')).json()
+    doc = doc_response.get('response', {}).get('docs', [None])[0]
+    if doc is None:
+        return None, []  # same diff
+
+    keys = get_topics_sorted_keys(doc)
+
+    sort = 'dist(2, vector(%s), vector(%s))' % (", ".join(keys), ", ".join(['%.4f' % doc[key] for key in keys]))
+
+    params = {'wt':'json', 
+              #'q':'-id:%s AND (%s)' % (doc['id'], " OR ".join(['(%s:*)' % key for key in keys])),
+              'q':'*:*',
+              'sort': sort + ' asc',
+              'rows':20, 
+              'fl':'id,sitename_txt,topic_*,wam_i,url,'+sort}
+
+    docs = requests.get('%s/select/' % SOLR_URL, params=params).json().get('response', {}).get('docs', [])
+    map(lambda x: x.__setitem__('score', x[sort]), docs)
+
+    return (doc, docs)
+
 
 @app.route('/')
 def index():
     query = request.args.get('id')
     queried_doc = None
     if query is not None:
+        """
         queried_doc, docs = get_similar(query, 
                                         wam_boost=request.args.get('wam_boost'), 
                                         topic_boost=request.args.get('topic_boost', 1000), 
                                         delta=request.args.get('delta', 0.15),
                                         naive=bool(request.args.get('naive', False)),
                                         use_titles=bool(request.args.get('use_titles', False)))
+        """
+        queried_doc, docs = as_euclidean(query)
     else:
         docs = get_random_grouping()
-    docs = [doc for doc in docs if len(doc.keys()) > 2]  # dropping topicless wikis
+
+    #docs = [doc for doc in docs if len(get_topics_sorted_keys(doc)) > 0]  # dropping topicless wikis
     return render_template('index.html', docs=docs, queried_doc=queried_doc, qs=re.sub(r'id=\d+(&)?', '', request.query_string).replace('&&', '&'))
 
 app.debug = True
