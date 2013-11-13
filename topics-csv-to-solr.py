d91946d0e8217dc32610cdb507a7ad95d5c6dc6d
Robert Elwell <robert@wikia-inc.com>
2013-11-13 10:17:42 -0800

    added etl script

---
+++ topics-csv-to-solr.py
@@ -0,0 +1,14 @@
+import requests, sys, json
+
+docs = []
+with open(sys.argv[1], 'r') as fl:
+    for line in fl:
+        ploded = line[:-1].split(',')
+        wid = ploded[0]
+        doc = dict(id=wid)
+        for grouping in ploded[1:]:
+            topic, value = grouping.split('-')
+            doc['topic_%s_tf' % topic] = {'set': value}
+        docs += [doc]
+
+print requests.post('http://dev-search:8983/solr/xwiki/update?commit=true', data=json.dumps(docs), headers={'Content-type': 'application/json'}).content
