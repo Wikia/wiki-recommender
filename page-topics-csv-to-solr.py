49c2d820ca466dbb4dec8dc61f1a8a5fe84fb5f7
Robert Elwell <robert@wikia-inc.com>
2014-01-02 11:51:49 -0800

    batch updates

--- page-topics-csv-to-solr.py
+++ page-topics-csv-to-solr.py
@@ -1,24 +1,33 @@
-import requests, sys, json
+import requests
+import sys
+import json
 
-etl = len(sys.argv) > 2
-
-params = {'fl': 'id,title_en,url,wid,wam,backlinks', 'wt': 'json', 'rows': '1' }
+params = {'fl': 'id,title_en,url,wid,wam,backlinks', 'wt': 'json'}
 
 docs = []
 with open(sys.argv[1], 'r') as fl:
-    for line in fl:
-        ploded = line[:-1].split(',')
-        doc_id = ploded[0]
+    for i in range(0, len(fl), 100):
+        print "%d / %d" % (i, len(fl))
+        post_docs = []
         try:
-            if not etl:
-                raise ValueError()
-            params['q'] = 'id:'+doc_id
-            doc = requests.get('http://search-s10:8983/solr/main/select', params=params).json()['response']['docs']['0']
-        except:
-            doc = dict(id=doc_id)
-        for grouping in ploded[1:]:
-            topic, value = grouping.split('-')
-            doc['topic_%s_tf' % topic] = {'set': value}
-        docs += [doc]
+            params['q'] = ' OR '.join(['id:'+line.split(',')[0] for line in fl[i:i+100]])
+            docs = dict(
+                [(doc['id'], doc)
+                 for doc in requests.get('http://search-s10:8983/solr/main/select', params=params)
+                                    .json()['response']['docs']
+                 ]
+            )
+        except (ValueError, KeyError) as e:
+            print e
+        for line in fl[i:i+100]:
 
-print requests.post('http://dev-search:8983/solr/main/update?commit=true', data=json.dumps(docs), headers={'Content-type': 'application/json'}).content
+            ploded = line[:-1].split(',')
+            doc_id = ploded[0]
+            doc = docs.get(doc_id, {'id': doc_id})
+            for grouping in ploded[1:]:
+                topic, value = grouping.split('-')
+                doc['topic_%s_tf' % topic] = {'set': value}
+        post_docs += [doc]
+        print requests.post('http://dev-search:8983/solr/main/update?commit=true',
+                            data=json.dumps(post_docs),
+                            headers={'Content-type': 'application/json'}).content
