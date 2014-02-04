78639019665442874366b5994e84ef92981c1813
Robert Elwell <robert@wikia-inc.com>
2014-02-04 11:24:04 -0800

    mouseover topic data my g

--- video-testapp.py
+++ video-testapp.py
@@ -1,10 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 import requests
-import sys
 import re
 import random
 import json
-import time
 import os
 
 app = Flask(__name__)
@@ -24,19 +22,27 @@ def strip_file(doc_title):
 
 @app.template_filter('topics_sorted')
 def get_topics_sorted(doc):
-    return sorted([(key, doc[key]) for key in doc.keys() if re.match(r'topic_\d+_tf', key) is not None and doc[key] > 0], reverse=True, key=lambda x:x[1])
+    return sorted([(key, doc[key]) for key in doc.keys()
+                   if re.match(r'topic_\d+_tf', key) is not None and doc[key] > 0],
+                  reverse=True,
+                  key=lambda x: x[1])
+
 
 @app.template_filter('intersection_count')
 def intersection_count(tuples1, tuples2):
     return len([x for x in tuples1 if x[0] in [y[0] for y in tuples2]])
 
+
 @app.template_filter('topics_sorted_keys')
 def get_topics_sorted_keys(doc):
-    return sorted([key for key in doc.keys() if re.match(r'topic_\d+_tf', key) is not None and doc[key] > 0], reverse=True, key=lambda x:x[1])
+    return sorted([key for key in doc.keys() if re.match(r'topic_\d+_tf', key) is not None and doc[key] > 0],
+                  reverse=True,
+                  key=lambda x: x[1])
+
 
 @app.template_filter('append')
-def append(list, val):
-    return list + [val]
+def append(lst, val):
+    return lst + [val]
 
 
 def get_random_grouping():
@@ -45,35 +51,6 @@ def get_random_grouping():
     docs = requests.get('%s/select/' % SOLR_URL, params=params).json().get('response', {}).get('docs', [])
     return docs
 
-
-def get_similar_old(query, wam_boost=None, topic_boost='1000', delta=0.15, naive=False, use_titles=False):
-    doc_response = requests.get('%s/select/' % SOLR_URL, params=dict(rows=1, q='id:%s' % query, wt='json')).json()
-    doc = doc_response.get('response', {}).get('docs', [None])[0]
-    if doc is None:
-        return None, []  # same diff
-
-    """
-    Let's boost the query by the value of the topics, with boost
-    """
-    bf = ['map(%s, 0, 100, 0, 0)^%s' % (key, topic_boost) for key in doc.keys() if re.match(r'topic_\d+_tf', key) is not None]
-
-    if wam_boost is not None:
-        bf += ['wam_i^%s' % wam_boost]
-
-    if float(delta) != 0:
-        q = " OR ".join(['%s:[%.3f TO %.3f]' % (key, float(doc[key]) - float(delta), float(doc[key]) + float(delta)) for key in doc.keys() if re.match(r'topic_\d+_tf', key) is not None])
-    else:
-        q = '*:*'
-
-    params = dict(rows=20, q=q, bf=bf, wt='json', defType='edismax', fl='id,title_en,url,wid,topic_*')
-
-    if naive:
-        params['q'] = " OR ".join(['%s:[%.3f TO %.3f]' % (x[0], float(x[1]) - float(delta), float(x[1]) + float(delta)) for x in get_topics_sorted(doc)[:4]])
-        del params['bf']
-        if wam_boost is not None:
-            params['bf'] = ['wam_i^%s' % wam_boost]
-
-    return (doc, requests.get('%s/select/' % SOLR_URL, params=params).json().get('response', {}).get('docs', []))
 
 def as_euclidean(query):
     doc_response = requests.get('%s/select/' % SOLR_URL, params=dict(rows=1, q='id:%s' % query, wt='json')).json()
@@ -85,12 +62,12 @@ def as_euclidean(query):
 
     sort = 'dist(2, vector(%s), vector(%s))' % (", ".join(keys), ", ".join(['%.8f' % doc[key] for key in keys]))
 
-    params = {'wt':'json',
-              'q':'title_en:* AND is_video:true AND -(%s)' % " AND ".join(["%s:0" % key for key in keys]),
+    params = {'wt': 'json',
+              'q': 'title_en:* AND is_video:true AND -(%s)' % " AND ".join(["%s:0" % key for key in keys]),
               'sort': sort + ' asc',
-              'rows':20,
+              'rows': 20,
               'fq': '-id:%s' % doc['id'],
-              'fl':'id,wikititle_en,title_en,topic_*,wam,wid,url,'+sort}
+              'fl': 'id,wikititle_en,title_en,topic_*,wam,wid,url,'+sort}
 
     if request.args.get('nosame'):
         params['q'] += ' AND -wid:'+str(doc['wid'])
@@ -98,7 +75,14 @@ def as_euclidean(query):
     docs = requests.get('%s/select/' % SOLR_URL, params=params).json().get('response', {}).get('docs', [])
     map(lambda x: x.__setitem__('score', x[sort]), docs)
 
-    return (doc, docs)
+    return doc, docs
+
+
+@app.route('/topic_data.js')
+def topic_js():
+    global video_topic_data
+    return Response("var topic_data = %s;" % json.dumps(video_topic_data),
+                    mimetype="application/javascript", content_type="application/javascript")
 
 
 @app.route('/')
@@ -106,32 +90,31 @@ def index():
     query = request.args.get('id')
     queried_doc = None
     if query is not None:
-        """
-        queried_doc, docs = get_similar(query,
-                                        wam_boost=request.args.get('wam_boost'),
-                                        topic_boost=request.args.get('topic_boost', 1000),
-                                        delta=request.args.get('delta', 0.15),
-                                        naive=bool(request.args.get('naive', False)),
-                                        use_titles=bool(request.args.get('use_titles', False)))
-        """
         queried_doc, docs = as_euclidean(query)
     else:
         docs = get_random_grouping()
 
     details = {}
-    NO_IMAGE_URL = "http://slot1.images.wikia.nocookie.net/__cb62407/common/extensions/wikia/Search/images/wiki_image_placeholder.png"
-    for doc in docs:
-        items = [{}]
-        try:
-            items = requests.get(doc['url'].split('/wiki')[0]+'/api/v1/Articles/Details', 
-                                              params={'ids': doc['id'].split('_')[1]}).json().get('items', {123: {}}).values()
-        except:
-            pass
-        details[doc['id']] = items[0] if len(items) > 0 else {}
-        details[doc['id']]['thumbnail'] = details[doc['id']].get('thumbnail', NO_IMAGE_URL)
+    no_image_url = ("http://slot1.images.wikia.nocookie.net/"
+                    + "__cb62407/common/extensions/wikia/Search/images/wiki_image_placeholder.png")
 
-    return render_template('video_index.html', docs=docs, details=details, queried_doc=queried_doc, qs=re.sub(r'id=\d+(&)?', '', request.query_string).replace('&&', '&'))
+    items = requests.get(docs[0]['url'].split('/wiki')[0]+'/api/v1/Articles/Details',
+                         params={'ids': ','.join(doc['id'].split('_')[1] for doc in docs)}).json().get('items', {})
+
+    for doc in docs:
+        details[doc['id']] = items.get(doc['id'].split('_')[1], {})
+        details[doc['id']]['thumbnail'] = details[doc['id']].get('thumbnail', no_image_url)
+
+    return render_template('video_index.html',
+                           docs=docs,
+                           details=details,
+                           queried_doc=queried_doc,
+                           qs=re.sub(r'id=\d+(&)?', '', request.query_string).replace('&&', '&'))
+
+video_topic_data = None
 
 if __name__ == '__main__':
+    with open('video-999topics-words.txt', 'r') as topics_file:
+        video_topic_data = dict([('topic_%d_tf' % x[0], x[1][:-1]) for x in enumerate(topics_file)])
     app.debug = True
     app.run('0.0.0.0')
