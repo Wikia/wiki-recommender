e1272dee5e33771de954701a48045d048ec96e4d
Robert Elwell <robert@wikia-inc.com>
2014-02-18 16:30:15 -0800

    gotta reinitialize

--- topics-csv-to-solr.py
+++ topics-csv-to-solr.py
@@ -8,6 +8,9 @@ with open(sys.argv[1], 'r') as fl:
         ploded = line[:-1].split(',')
         wid = ploded[0]
         doc = dict(id=wid, has_topics_b=True)
+        #reinitialize
+        for i in range(0, 999):
+            doc['topic_%d_tf' % i] = 0
         for grouping in ploded[1:]:
             topic, value = grouping.split('-')
             doc['topic_%s_tf' % topic] = {'set': value}
