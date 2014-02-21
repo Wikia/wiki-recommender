59ec4da086bbeb0a10700b557a2649b20a41605a
Robert Elwell <robert@wikia-inc.com>
2014-02-20 16:20:00 -0800

    memory fix?

--- generate-wiki-recommendations-xls.py
+++ generate-wiki-recommendations-xls.py
@@ -1,14 +1,33 @@
 import sys
 import xlwt
 from lib.wikis import as_euclidean, get_wikis_with_topics, csv_to_solr
+from multiprocessing import Pool
 
 
 SOLR_URL = 'http://dev-search:8983/solr/xwiki'
 
 
+def get_recommendations(wiki):
+    wiki_id = wiki['id']
+    print wiki_id
+    try:
+        wiki_doc, recommendations = as_euclidean(wiki_id)
+        wiki_doc['recommendation_ids'] = [x.get('id', '?') for x in recommendations]
+        wiki_doc['recommendation_urls'] = [x.get('url', '?') for x in recommendations]
+        wiki_doc['recommendation_sitenames'] = [' '.join(x.get('sitename_txt', ['?'])) for x in recommendations]
+        if 'url' not in wiki_doc:
+            return None
+        return wiki_doc
+    except ValueError as e:
+        print e
+        return None
+
+
 def main():
+    name = 'latest'
     if len(sys.argv) >= 2:
         print "Reinitializing Solr with desired topics CSV"
+        name = sys.argv[1].split('.')[0]
         with open(sys.argv[1], 'r') as fl:
             print csv_to_solr(fl)
 
@@ -25,31 +44,23 @@ def main():
     names_worksheet.write(0, 0, 'Wiki')
     names_worksheet.write(0, 1, 'Recommendations')
 
-    i = 1
-    for wiki in get_wikis_with_topics():
-        wiki_id = wiki['id']
-        print wiki_id
-        try:
-            wiki_doc, recommendations = as_euclidean(wiki_id)
-        except ValueError as e:
-            print e
-            continue
-        if 'url' not in wiki_doc or not recommendations:
-            continue
+    pool = Pool(processes=4)
+    recommendations_results = filter(lambda x: x, pool.map_async(get_recommendations, get_wikis_with_topics()).get())
+    for counter, wiki_doc in enumerate(recommendations_results):
+        i = counter + 1
         ids_worksheet.write(i, 0, wiki_doc['id'])
-        map(lambda j: ids_worksheet.write(i, j, recommendations[j-1].get('id', '?')),
-            range(1, len(recommendations)+1))
+        map(lambda j: ids_worksheet.write(i, j, wiki_doc['recommendation_ids'][j-1]),
+            range(1, len(wiki_doc['recommendations_ids'])+1))
 
         urls_worksheet.write(i, 0, wiki_doc['url'])
-        map(lambda j: urls_worksheet.write(i, j, recommendations[j-1].get('url', '?')),
-            range(1, len(recommendations)+1))
+        map(lambda j: urls_worksheet.write(i, j, wiki_doc['recommendation_urls'][j-1]),
+            range(1, len(wiki_doc['recommendations_urls'])+1))
 
         names_worksheet.write(i, 0, ''.join(wiki_doc['sitename_txt']))
-        map(lambda j: names_worksheet.write(i, j, ''.join(recommendations[j-1].get('sitename_txt', ['?']))),
-            range(1, len(recommendations)+1))
-        i += 1
+        map(lambda j: names_worksheet.write(i, j, wiki_doc['recommendations_sitenames'][j-1]),
+            range(1, len(wiki_doc['recommendations_sitenames'])+1))
 
-    my_workbook.save(sys.argv[1].split('.')[0]+"-wiki-recommendations.xls")
+    my_workbook.save(name+"-wiki-recommendations.xls")
 
 
 if __name__ == '__main__':
