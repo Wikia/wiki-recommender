0e6f154bbe8eceda94eee12aa087a0e0d9980932
Robert Elwell <robert@wikia-inc.com>
2014-02-24 15:12:43 -0800

    improvements to borda ranking

---
+++ xls-to-borda-rank.py
@@ -0,0 +1,89 @@
+import sys
+import requests
+import xlrd
+import xlwt
+from datetime import datetime
+from collections import defaultdict
+from multiprocessing import Pool
+
+
+def borda(list_of_lists):
+    dct = defaultdict(list)
+    for li in list_of_lists:
+        for rank, val in list(enumerate(li)):
+            dct[val].append(rank)
+
+    lowest_rank = max([rank for val, ranks in dct.items() for rank in ranks]) + 1
+
+    # 1000000 is an arbtrarily big number for instances that aren't included in borda
+    normalized = [(val, sum(ranks + [lowest_rank] * (len(list_of_lists) * len(ranks)))) for val, ranks in dct.items()]
+
+    return sorted(normalized, key=lambda x: x[1])
+
+
+def wiki_data_for_ids(ids):
+    return requests.get('http://www.wikia.com/api/v1/Wikis/Details',
+                        params={'ids': ','.join(ids)}).json().get('items', {})
+
+
+def dict_from_args():
+    doc_to_recs = defaultdict(list)
+    for arg in sys.argv[1:]:
+        book = xlrd.open_workbook(arg)
+        sheet = book.sheets()[0]
+        for i in range(1, sheet.nrows):
+            row_vals = sheet.row_values(i)
+            doc_to_recs[row_vals[0]].append(row_vals[1:])
+    return doc_to_recs
+
+
+def main():
+    print "Combining recommendations"
+    borda_recs = []
+    borda_vals = dict()
+    for doc_id, recs in dict_from_args().items():
+        individual_borda_recs = borda(recs)
+        borda_vals[doc_id] = dict(individual_borda_recs)
+        borda_recs.append([doc_id] + [x[0] for x in individual_borda_recs])
+
+    print "Getting wiki data"
+    pool = Pool(processes=4)
+    ids = list(set([col for row in borda_recs for col in row]))
+    wiki_data = {}
+    r = pool.map_async(wiki_data_for_ids, [ids[i:i+20] for i in range(0, len(ids), 20)])
+    map(wiki_data.update, r.get())
+
+    print "Writing Data"
+    my_workbook = xlwt.Workbook()
+    ids_worksheet = my_workbook.add_sheet("Wiki IDs")
+    ids_worksheet.write(0, 0, 'Wiki')
+    ids_worksheet.write(0, 1, 'Recommendations')
+
+    urls_worksheet = my_workbook.add_sheet("Wiki URLs")
+    urls_worksheet.write(0, 0, 'Wiki')
+    urls_worksheet.write(0, 1, 'Recommendations')
+
+    names_worksheet = my_workbook.add_sheet("Wiki Names")
+    names_worksheet.write(0, 0, 'Wiki')
+    names_worksheet.write(0, 1, 'Recommendations')
+
+    seeds_worksheet = my_workbook.add_sheet("Individual Recommendation Files")
+    for counter, arg in enumerate(sys.argv[1:]):
+        seeds_worksheet.write(counter, 0, arg)
+
+    borda_recs = sorted(borda_recs, key=lambda y: wiki_data.get(y[0], {}).get('wam_score', 0), reverse=True)
+    for counter, line in enumerate(borda_recs):
+        row = counter + 1
+        for col in range(0, len(line)):
+            ids_worksheet.write(row, col, line[col])
+            urls_worksheet.write(row, col, wiki_data.get(line[col], {}).get('url', '?'))
+            names_worksheet.write(row, col, "%s (%s)" % (wiki_data.get(line[col], {}).get('title', '?'),
+                                                         str(borda_vals[line[0]].get(line[col], '?'))))
+
+    fname = 'combined-recommendations-%s.xls' % (datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M'))
+    my_workbook.save(fname)
+    print fname
+
+
+if __name__ == '__main__':
+    main()
