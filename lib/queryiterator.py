c0f4d209ee418f6c7c5cf0c8a44acbdf47c5c3d8
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-12 23:02:18 +0000

    QueryIterator works but no concurrency yet

--- lib/queryiterator.py
+++ lib/queryiterator.py
@@ -1,4 +1,5 @@
 import requests
+from wikis import as_euclidean
 
 
 class QueryIterator(object):
@@ -6,7 +7,7 @@ class QueryIterator(object):
         if core not in ('xwiki', 'main'):
             raise ValueError('core must be "xwiki" or "main"')
 
-        self.host = 'http://%s:8983/solr/%s/select' % (server, core)
+        self.host = 'http://%s:8983/solr/%s' % (server, core)
         self.query = 'has_topics_b:true'
         if query is not None:
             self.query = '%s AND ' % query + self.query
@@ -33,11 +34,8 @@ class QueryIterator(object):
     def get_more_docs(self):
         if self.num_found is not None and self.start >= self.num_found:
             raise StopIteration
-        response = requests.get(self.host, params=self.get_params(),
-                                timeout=300).json()
-        #response = requests.get(self.host, params=self.get_params(),
-        #                        timeout=300).content
-        #print response
+        response = requests.get(self.host + '/select',
+                                params=self.get_params(), timeout=300).json()
         self.num_found = response['response']['numFound']
         self.docs = response['response']['docs']
         self.start += self.rows
@@ -47,9 +45,20 @@ class QueryIterator(object):
         if (len(self.docs) == 0):
             self.get_more_docs()
         self.at += 1
-        return self.docs.pop()
+        try:
+            doc, recommendations = as_euclidean(self.docs.pop()['id'], self.host)
+            doc['recommendation_ids'] = [x.get('id', '?') for x in recommendations]
+            doc['recommendation_urls'] = [x.get('url', '?') for x in recommendations]
+            doc['recommendation_sitenames'] = [' '.join(x.get('sitename_txt', ['?'])) for x in recommendations]
+            if 'url' not in doc:
+                return None
+            return doc
+        except ValueError as e:
+            print e
+            return None
+        #return self.docs.pop()
 
 if __name__ == '__main__':
     q = QueryIterator('lang_s:en')
-    for i in q:
-        print i
+    for doc in q:
+        print '%s, %s' % (doc['sitename_txt'][0], doc['recommendation_sitenames'])
