0ab2afb9f896c1b95ff000383aaa1c6de215ec0c
Robert Elwell <robert@wikia-inc.com>
2014-03-12 09:44:05 -0700

    trying with multiprocessing

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,27 +1,13 @@
 import requests
 import json
-from multiprocessing import Pool
+from multiprocessing import Pool, Value
 
 
+def process_linegroup(tup):
+    endpoint, shared_counter, initialize_doc, linegroup = tup
 
-def send_update_docs_to_endpoint(endpoint, update_docs):
-    requests.post('%s/update' % endpoint,
-                  data=json.dumps(update_docs),
-                  headers={'Content-type': 'application/json'})
-
-
-def csv_to_solr(fl, endpoint='http://dev-search:8983/solr/main', num_topics=999, reset_callback=None):
-
-    if reset_callback:
-        print "Resetting (no way back now!)"
-        reset_callback()
-
-    print 'generating update docs'
     update_docs = []
-    counter = 0
-    initialize_doc = dict([('topic_%d_tf' % i, {'set': 0}) for i in range(1, num_topics)])
-    p = Pool(processes=8)
-    for line in fl:
+    for line in linegroup:
         ploded = line[:-1].split(',')
         wid = ploded[0]
         # initialize with no topics
@@ -32,14 +18,33 @@ def csv_to_solr(fl, endpoint='http://dev-search:8983/solr/main', num_topics=999,
                          for topic, value in
                          [tuple(grouping.split('-')) for grouping in ploded[1:]]]))
         update_docs.append(doc)
-        if len(update_docs) >= 10000:
-            counter += len(update_docs)
-            print counter
-            p.apply_async(send_update_docs_to_endpoint, update_docs)
-            update_docs = []
 
-    requests.post('%s/update?commit=true' % endpoint,
-                  data=json.dumps(update_docs),
-                  headers={'Content-type': 'application/json'})
+    shared_counter.value += len(update_docs)
+    print shared_counter.value
+
+    return requests.post('%s/update' % endpoint,
+                         data=json.dumps(update_docs),
+                         headers={'Content-type': 'application/json'})
+
+
+def csv_to_solr(fl, endpoint='http://dev-search:8983/solr/main', num_topics=999, reset_callback=None):
+
+    if reset_callback:
+        print "Resetting (no way back now!)"
+        reset_callback()
+
+    print 'generating updates'
+    initialize_doc = dict([('topic_%d_tf' % i, {'set': 0}) for i in range(1, num_topics)])
+    p = Pool(processes=8)
+    counter = Value()
+    lines = [line for line in fl]
+    groupings = [(endpoint, counter, initialize_doc, lines[i:i+10000]) for i in range(0, len(lines), 10000)]
+    del lines  # save dat memory my g
+
+    print 'processing line groups'
+    p.map_async(groupings)
+
+    print "Committing..."
+    requests.post('%s/update?commit=true' % endpoint)
 
     return True
