2feff8854b044ebb4c4c544df9caee7649504504
Robert Elwell <robert@wikia-inc.com>
2014-03-12 09:07:37 -0700

    reset first, then extract

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,24 +5,30 @@ import json
 def csv_to_solr(fl, endpoint='http://dev-search:8983/solr/main', num_topics=999, reset_callback=None):
     update_docs = []
 
-    print 'generating update docs'
+    if reset_callback:
+        print "Resetting (no way back now!)"
+        reset_callback()
 
+    print 'generating update docs'
+    counter = 0
     for line in fl:
         ploded = line[:-1].split(',')
         wid = ploded[0]
-        doc = dict(id=wid, has_topics_b={'set': True})
-        # initialize
-        for i in range(0, num_topics):
-            doc['topic_%d_tf' % i] = {'set': 0}
+        # initialize with no topics
+        doc = dict([('topic_%d_tf' % i, {'set': 0}) for i in range(1, 1000)])
+        doc.update(dict(id=wid, has_topics_b={'set': True}))
+        # overwrite with topics
         doc.update(dict([('topic_%s_tf' % topic, {'set': value})
                          for topic, value in
                          [tuple(grouping.split('-')) for grouping in ploded[1:]]]))
         update_docs.append(doc)
-
-    print 'got update docs'
-
-    if reset_callback:
-        reset_callback()
+        if len(update_docs) >= 10000:
+            counter += len(update_docs)
+            print counter
+            requests.post('%s/update?commit=true' % endpoint,
+                          data=json.dumps(update_docs),
+                          headers={'Content-type': 'application/json'})
+            update_docs = []
 
     requests.post('%s/update?commit=true' % endpoint,
                   data=json.dumps(update_docs),
