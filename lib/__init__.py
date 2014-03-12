43a3920e8dab55e931ad428a41cba7229e6d2dce
Robert Elwell <robert@wikia-inc.com>
2014-03-12 10:32:33 -0700

    stupid len

--- lib/__init__.py
+++ lib/__init__.py
@@ -40,7 +40,7 @@ def csv_to_solr(fl, endpoint='http://dev-search:8983/solr/main', num_topics=999,
     grouping_counter = 0
     for line in fl:
         line_groupings[grouping_counter].append(line)
-        if len(line_groupings[grouping_counter] > 10000):
+        if len(line_groupings[grouping_counter]) > 10000:
             if grouping_counter == 7:
                 print 'processing line groups for', sum(map(len, line_groupings)), 'lines'
                 groupings = [(endpoint, initialize_doc, line_groupings[i]) for i in range(0, len(line_groupings))]
