78885c106177ff83567a95f9b5fefb19532e843d
Robert Elwell <robert@wikia-inc.com>
2014-03-12 09:11:58 -0700

    lol speedup loopz

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,7 +3,6 @@ import json
 
 
 def csv_to_solr(fl, endpoint='http://dev-search:8983/solr/main', num_topics=999, reset_callback=None):
-    update_docs = []
 
     if reset_callback:
         print "Resetting (no way back now!)"
@@ -11,12 +10,14 @@ def csv_to_solr(fl, endpoint='http://dev-search:8983/solr/main', num_topics=999,
 
     print 'generating update docs'
     counter = 0
+    update_docs = []
+    initialize_doc = dict([('topic_%d_tf' % i, {'set': 0}) for i in range(1, 1000)])
     for line in fl:
         ploded = line[:-1].split(',')
         wid = ploded[0]
         # initialize with no topics
-        doc = dict([('topic_%d_tf' % i, {'set': 0}) for i in range(1, 1000)])
-        doc.update(dict(id=wid, has_topics_b={'set': True}))
+        doc = dict(id=wid, has_topics_b={'set': True})
+        doc.update(initialize_doc)
         # overwrite with topics
         doc.update(dict([('topic_%s_tf' % topic, {'set': value})
                          for topic, value in
