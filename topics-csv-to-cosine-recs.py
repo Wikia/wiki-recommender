ef9a72c11f72adfd3c6aea73c806d044d5caa9ee
Robert Elwell <robert@wikia-inc.com>
2014-03-14 15:52:29 -0700

    iteration, lame

--- topics-csv-to-cosine-recs.py
+++ topics-csv-to-cosine-recs.py
@@ -31,7 +31,7 @@ def get_recommendations(args, docid_to_topics):
     print "Getting all pairwise relations"
     pairwise = {}
     counter = 0
-    product = itertools.product(keys, keys)
+    product = [p for p in itertools.product(keys, keys)]
     ln = len(product)
     print "Product is", ln, "pairs"
     for k in product:
