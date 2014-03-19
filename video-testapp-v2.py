885c84d02872b0a2dca808ed1fac7671edc8f64d
Robert Elwell <robert@wikia-inc.com>
2014-03-19 14:11:10 -0700

    video 2 testapp for denormalized recommendations

---
+++ video-testapp-v2.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, render_template, Response
+from lib.filters import get_topics_sorted_keys, intersection_count, get_topics_sorted, strip_file, append
+from lib.querying import get_by_id
+from argparse import ArgumentParser
+import requests
+import re
+import random
+import json
+import os
+
+
+app = Flask(__name__)
+
+
+def get_args():
+    ap = ArgumentParser()
+    ap.add_argument('--solr_url', dest='solr_url', default='http://dev-search:8983/solr/main')
+    return ap.parse_args()
+
+
+def get_random_grouping():
+    params = dict(rows=25, q='is_video:true', sort='views desc',
+                  wt='json', fl='id,title_en,topic_*,url,wid,wikititle_en')
+    params['start'] = request.args.get('start', int(random.randint(0, 50000)))
+    docs = requests.get('%s/select/' % SOLR_URL, params=params).json().get('response', {}).get('docs', [])
+    return docs
+
+
+def get_for_ids(docids):
+    params = " OR ".join(["id:%s" % docid for docid in docids])
+    docs = requests.get('%s/select/' % SOLR_URL, params=params).json().get('response', {}).get('docs', [])
+    docs_sorted = [doc for doc in docs for docid in docids if docid == doc['id']]
+    print docids
+    print [d['id'] for d in docs_sorted]
+    return docs_sorted
+
+
+@app.route('/')
+def index():
+    query = request.args.get('id')
+    queried_doc = None
+    if query is not None:
+        docs = get_by_id(query)
+    else:
+        docs = get_random_grouping()
+
+    details = {}
+    no_image_url = ("http://slot1.images.wikia.nocookie.net/"
+                    + "__cb62407/common/extensions/wikia/Search/images/wiki_image_placeholder.png")
+
+    items = requests.get(docs[0]['url'].split('/wiki')[0]+'/api/v1/Articles/Details',
+                         params={'ids': ','.join(doc['id'].split('_')[1] for doc in docs)}).json().get('items', {})
+
+    for doc in docs:
+        details[doc['id']] = items.get(doc['id'].split('_')[1], {})
+        details[doc['id']]['thumbnail'] = details[doc['id']].get('thumbnail', no_image_url)
+
+    return render_template('video_index.html',
+                           docs=docs,
+                           details=details,
+                           queried_doc=queried_doc,
+                           qs=re.sub(r'id=\d+(&)?', '', request.query_string).replace('&&', '&'))
+
+
+def main():
+    global SOLR_URL, video_topic_data, app
+
+    SOLR_URL = get_args().solr_url
+
+    app.debug = True
+    app.add_template_filter(get_topics_sorted_keys, 'topics_sorted_keys')
+    app.add_template_filter(intersection_count, 'intersection_count')
+    app.add_template_filter(append, 'append')
+    app.add_template_filter(strip_file, 'strip_file')
+    app.add_template_filter(get_topics_sorted, 'topics_sorted')
+    app.run('0.0.0.0')
+
+if __name__ == '__main__':
+    video_topic_data = None
+    main()
